771219855a2e6e197ca800cc29c78f606f2f4944
Teodor Fredriksson <90504386+TeodorFredriksson95@users.noreply.github.com>
2025-12-31 22:08:41 +0100

    Initial Unreal Engine 5 project

---
+++ Source/LearnUECPP/LearnUECPP.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "LearnUECPP.h"
+
+#include "FloatingActor.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LearnUECPP, "LearnUECPP" );
---
+++ Source/LearnUECPP/SphereFloatingActor.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "FloatingActor.h"
+#include "SphereFloatingActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEARNUECPP_API ASphereFloatingActor : public AFloatingActor
+{
+	GENERATED_BODY()
+	
+};
